f11b9d10bdffaac067751df7c155514da3e69cf7
Youcef <youcef.kebaili@futurocom.com>
2021-04-14 18:25:33 +0200

    Ajout des valeurs de variables sur CActionWeb

--- timos.data/Aspectize/CActionWeb.cs
+++ timos.data/Aspectize/CActionWeb.cs
@@ -22,6 +22,7 @@ namespace timos.data.Aspectize
         public const string c_champLibelle = "Libelle";
         public const string c_champInstructions = "Instructions";
 
+        // Vairables de type Texte (ou Decimal)
         public const string c_champIdVarText1 = "IdVarText1";
         public const string c_champLabelVarText1 = "LabelVarText1";
         public const string c_champIdVarText2 = "IdVarText2";
@@ -32,14 +33,26 @@ namespace timos.data.Aspectize
         public const string c_champLabelVarText4 = "LabelVarText4";
         public const string c_champIdVarText5 = "IdVarText5";
         public const string c_champLabelVarText5 = "LabelVarText5";
+        // Valeurs possibles des variables texte
+        public const string c_champValeursVarText1 = "ValeursVarText1";
+        public const string c_champValeursVarText2 = "ValeursVarText2";
+        public const string c_champValeursVarText3 = "ValeursVarText3";
+        public const string c_champValeursVarText4 = "ValeursVarText4";
+        public const string c_champValeursVarText5 = "ValeursVarText5";
 
+        // Vairables de type Entier
         public const string c_champIdVarInt1 = "IdVarInt1";
         public const string c_champLabelVarInt1 = "LabelVarInt1";
         public const string c_champIdVarInt2 = "IdVarInt2";
         public const string c_champLabelVarInt2 = "LabelVarInt2";
         public const string c_champIdVarInt3 = "IdVarInt3";
         public const string c_champLabelVarInt3 = "LabelVarInt3";
+        // Valeurs possibles des variables texte
+        public const string c_champValeursVarInt1 = "ValeursVarInt1";
+        public const string c_champValeursVarInt2 = "ValeursVarInt2";
+        public const string c_champValeursVarInt3 = "ValeursVarInt3";
 
+        // Variables de type Date
         public const string c_champIdVarDate1 = "IdVarDate1";
         public const string c_champLabelVarDate1 = "LabelVarDate1";
         public const string c_champIdVarDate2 = "IdVarDate2";
@@ -47,6 +60,7 @@ namespace timos.data.Aspectize
         public const string c_champIdVarDate3 = "IdVarDate3";
         public const string c_champLabelVarDate3 = "LabelVarDate3";
 
+        // Variables de type Bool
         public const string c_champIdVarBool1 = "IdVarBool1";
         public const string c_champLabelVarBool1 = "LabelVarBool1";
         public const string c_champIdVarBool2 = "IdVarBool2";
@@ -73,7 +87,12 @@ namespace timos.data.Aspectize
             foreach (DataColumn col in dt.Columns)
             {
                 if (col.DataType == typeof(string))
-                    row[col] = "hidden";
+                {
+                    if (col.ColumnName.Contains("Label"))
+                        row[col] = "hidden";
+                    else
+                        row[col] = "";
+                }
             }
 
             if(processDb != null)
@@ -121,41 +140,60 @@ namespace timos.data.Aspectize
                             CVariableDynamique variable = wndVariable.Variable as CVariableDynamique;
                             if (wndVariable.WebLabel != "" && variable != null && variable is CVariableDynamiqueSaisie)
                             {
-                                switch (((CVariableDynamiqueSaisie)variable).TypeDonnee2i.TypeDonnee)
+                                CVariableDynamiqueSaisie variableSaisie = (CVariableDynamiqueSaisie)variable;
+                                bool bIsChoixParmis = variableSaisie.IsChoixParmis();
+                                StringBuilder sbValeurs = new StringBuilder();
+                                if (bIsChoixParmis)
+                                {
+                                    foreach (CValeurVariableDynamiqueSaisie val in variableSaisie.Valeurs)
+                                        sbValeurs.Append(val.Value + "#" + val.Display + "#");
+                                    sbValeurs.Remove(sbValeurs.Length-1, 1);
+                                }
+                                //string valeurs = sbValeurs.ToString();
+
+                                switch (variableSaisie.TypeDonnee2i.TypeDonnee)
                                 {
                                     case TypeDonnee.tEntier:
                                         if (nIndexIntVar <= 3)
                                         {
                                             row["IdVarInt" + nIndexIntVar] = variable.IdVariable;
-                                            row["LabelVarInt" + nIndexIntVar++] = wndVariable.WebLabel;
+                                            row["LabelVarInt" + nIndexIntVar] = wndVariable.WebLabel;
+                                            row["ValeursVarInt" + nIndexIntVar] = sbValeurs.ToString();
+                                            nIndexIntVar++;
                                         }
                                         break;
                                     case TypeDonnee.tDouble:
                                         if (nIndexTextVar <= 5)
                                         {
                                             row["IdVarText" + nIndexTextVar] = variable.IdVariable;
-                                            row["LabelVarText" + nIndexTextVar++] = wndVariable.WebLabel;
+                                            row["LabelVarText" + nIndexTextVar] = wndVariable.WebLabel;
+                                            row["ValeursVarText" + nIndexTextVar] = sbValeurs.ToString();
+                                            nIndexTextVar++;
                                         }
                                         break;
                                     case TypeDonnee.tString:
                                         if (nIndexTextVar <= 5)
                                         {
                                             row["IdVarText" + nIndexTextVar] = variable.IdVariable;
-                                            row["LabelVarText" + nIndexTextVar++] = wndVariable.WebLabel;
+                                            row["LabelVarText" + nIndexTextVar] = wndVariable.WebLabel;
+                                            row["ValeursVarText" + nIndexTextVar] = sbValeurs.ToString();
+                                            nIndexTextVar++;
                                         }
                                         break;
                                     case TypeDonnee.tDate:
                                         if (nIndexDateVar <= 3)
                                         {
                                             row["IdVarDate" + nIndexDateVar] = variable.IdVariable;
-                                            row["LabelVarDate" + nIndexDateVar++] = wndVariable.WebLabel;
+                                            row["LabelVarDate" + nIndexDateVar] = wndVariable.WebLabel;
+                                            nIndexDateVar++;
                                         }
                                         break;
                                     case TypeDonnee.tBool:
                                         if (nIndexBoolVar <= 3)
                                         {
                                             row["IdVarBool" + nIndexBoolVar] = variable.IdVariable;
-                                            row["LabelVarBool" + nIndexBoolVar++] = wndVariable.WebLabel;
+                                            row["LabelVarBool" + nIndexBoolVar] = wndVariable.WebLabel;
+                                            nIndexBoolVar++;
                                         }
                                         break;
                                     case TypeDonnee.tObjetDonneeAIdNumeriqueAuto:
@@ -218,12 +256,22 @@ namespace timos.data.Aspectize
             dt.Columns.Add(c_champIdVarText5, typeof(string));
             dt.Columns.Add(c_champLabelVarText5, typeof(string));
 
+            dt.Columns.Add(c_champValeursVarText1, typeof(string));
+            dt.Columns.Add(c_champValeursVarText2, typeof(string));
+            dt.Columns.Add(c_champValeursVarText3, typeof(string));
+            dt.Columns.Add(c_champValeursVarText4, typeof(string));
+            dt.Columns.Add(c_champValeursVarText5, typeof(string));
+
             dt.Columns.Add(c_champIdVarInt1, typeof(string));
             dt.Columns.Add(c_champLabelVarInt1, typeof(string));
             dt.Columns.Add(c_champIdVarInt2, typeof(string));
             dt.Columns.Add(c_champLabelVarInt2, typeof(string));
             dt.Columns.Add(c_champIdVarInt3, typeof(string));
             dt.Columns.Add(c_champLabelVarInt3, typeof(string));
+
+            dt.Columns.Add(c_champValeursVarInt1, typeof(string));
+            dt.Columns.Add(c_champValeursVarInt2, typeof(string));
+            dt.Columns.Add(c_champValeursVarInt3, typeof(string));
 
             dt.Columns.Add(c_champIdVarDate1, typeof(string));
             dt.Columns.Add(c_champLabelVarDate1, typeof(string));
